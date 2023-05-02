6d14c89078e9d1b49e10c7bf02d853d35926876a
SuryaRaghuvanshi02 <73468842+SuryaRaghuvanshi02@users.noreply.github.com>
2023-05-02 21:06:00 +0530

    1st commit

---
+++ client/src/Post.js
@@ -0,0 +1,15 @@
+export default function Post(){
+    return(
+        <div className="post">
+          <div className="image"><img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*esLAtPgM_LTWzPYQfCQr8g.jpeg" alt="" /></div>
+          <div className="texts">
+            <h2>Link Prediction on Distributed Environment: A Comparison between Support Vector Machines and Neural Networks</h2>
+            <p className="info">
+              <a href="" className="autor">Vansh Gupta</a>
+              <time>2023-04-25 16:45</time>
+            </p>
+            <p className="summary">Websites are a ubiquitous aspect of our everyday lives in the modern world, from social media to itinerary.</p>
+          </div>
+        </div>
+    );
+}
